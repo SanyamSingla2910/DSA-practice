77ffe86b9fb3bdb4c0fe001777d88385fe0159a7
SanyamSingla2910 <singlasanyam2910@gmail.com>
2026-06-05 13:54:29 +0530

    Adding my dsa notes

---
+++ C++ Basics/LearnCPP.cpp
@@ -0,0 +1,128 @@
+// This File contain the basics of cpp and i wrote single line of code for every basic cpp fundamental 
+#include <bits/stdc++.h>
+using namespace std;
+
+void printname(string name){
+    cout<<name;
+}
+
+int twosum(int n1,int n2){
+    int n3 =n1+n2;
+    return n3;
+}
+
+int main(){
+    // int y;
+    // cin>>y;
+    // cout<<y<<endl;
+    // long long x;
+    // cin>>x;
+    // cout<<x;
+
+    // string str;
+    // getline(cin , str);
+    // cout<<str;
+
+    // float x;
+    // cin>>x;
+    // cout<<x;
+
+    // char ch;
+    // cin>>ch;
+    // cout<<ch;
+
+    // int age;
+    // cin>>age;
+    // if (age>=18){
+    //     cout<<"You are an adult";
+    // }else{
+    //     cout<<"Not an adult";
+    // }
+
+    // int grade;
+    // cin>>grade;
+    // if (grade <25){
+    //     cout<<"F";
+    // }
+    // else if (grade<=44 ){
+    //     cout<<"E";
+    // }
+    // else if (grade<=49 ){
+    //     cout<<"D";
+    // }
+    // else if (grade<=59 ){
+    //     cout<<"C";
+    // }
+    // else if (grade<=79 ){
+    //     cout<<"B";
+    // }
+    // else if (grade<=100 ){
+    //     cout<<"A";
+    // }
+
+    // int day;
+    // cin>>day;
+    // switch(day){
+    //     case 1:
+    //         cout<<"Monday";
+    //     case 2:
+    //         cout<<"Tuesday";
+    //         break;
+    //     case 3:
+    //         cout<<"Wednseday";
+    //         break;
+    //     case 4:
+    //         cout<<"Thrusday";
+    //         break;
+    //     case 5:
+    //         cout<<"Friday";
+    //         break;
+    //     case 6:
+    //         cout<<"Saturday";
+    //         break;
+    //     case 7:
+    //         cout<<"Sunday";
+    //         break;
+    //     default:
+    //         cout<<"invalid";
+    // }
+    // int arr[3];
+    // cin>>arr[0]>>arr[1]>>arr[2];
+    // cout<<arr[1];
+
+    // int arr[3][5];
+    // // 2d array
+    // return 0;
+
+    // for(int i=0;i<10;i++){
+    //     // Like if i want to print my name 10 times
+    //     cout<<"Sanyam"<<endl;
+    // }
+
+    // int i=0;
+    // while(i<10){
+    //     cout<<"DSA is Powerful"<<endl;
+    //     i=i+1;
+    // }
+
+    // functions 
+
+    // string Name = "Sanyam";
+    // printname(Name);
+    // cout<<endl;
+    // string Name2 = "Mohit";
+    // printname(Name2);
+    // return 0;
+
+    // int x = twosum(2,4);
+    // int y = twosum(21241,135135);
+    // cout<<x<<endl<<y<<endl;
+
+    
+    // int x = min(1,2);
+    // int y = max(1,2);
+
+    // cout<<x<<endl<<y<<endl;
+
+    
+}
